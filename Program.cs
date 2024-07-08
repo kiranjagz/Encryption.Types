761ff444d747d335f56333da36dc6f4372c9d705
Kiran <kiranjagz@gmai.com>
2024-07-08 22:52:13 +0200

    feat: added aes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Encryption.Types.ZonePinKey;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 class Program
@@ -7,25 +6,80 @@ class Program
     static void Main(string[] args)
     {
         string zpk = "0123456789ABCDEF0123456789ABCDEF";
+        string newZpk = "0123456789ABCDEF0123456789ABCDEG";
+        List<string> list = [zpk, newZpk];
+
 
         // Example PIN
         string pin = "1357";
 
-        var tripleDesService = new TripleDesService();
-        var zonePinKeyService = new ZonePinKeyService();
+        foreach (var item in list)
+        {
+            // Encrypt the PIN
+            string encryptedPin = Encrypt(pin, item);
+            Console.WriteLine($"Encrypted PIN: {encryptedPin}");
 
-        // Encrypt the PIN
-        string encryptedPin = tripleDesService.Encrypt(pin, zpk);
-        Console.WriteLine($"This is 3DES Encrypted PIN: {encryptedPin}");
-
-        // Decrypt the PIN
-        string decryptedPin = tripleDesService.Decrypt(encryptedPin, zpk);
-        Console.WriteLine($"This is 3DES Decrypted PIN: {decryptedPin}");
-
-        // Encrypt the PIN
-        string encryptedPinZPK = zonePinKeyService.Encrypt(pin, zpk);
-        Console.WriteLine($"This is ZPK Encrypted PIN: {encryptedPinZPK}");
+            // Decrypt the PIN
+            string decryptedPin = Decrypt(encryptedPin, item);
+            Console.WriteLine($"Decrypted PIN: {decryptedPin}");
+        }
     }
 
+    public static string Encrypt(string data, string key)
+    {
+        using (var aes = new AesManaged())
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            Array.Resize(ref keyBytes, aes.KeySize / 8); // Adjust the key size
+            aes.Key = keyBytes;
 
+            aes.GenerateIV();
+            var iv = aes.IV;
+            var encryptor = aes.CreateEncryptor(aes.Key, iv);
+
+            using (var ms = new System.IO.MemoryStream())
+            {
+                ms.Write(iv, 0, iv.Length);
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (var sw = new System.IO.StreamWriter(cs))
+                    {
+                        sw.Write(data);
+                    }
+                }
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+    }
+
+    public static string Decrypt(string encryptedData, string key)
+    {
+        byte[] fullCipher = Convert.FromBase64String(encryptedData);
+
+        using (var aes = new AesManaged())
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            Array.Resize(ref keyBytes, aes.KeySize / 8); // Adjust the key size
+            aes.Key = keyBytes;
+
+            byte[] iv = new byte[aes.BlockSize / 8];
+            byte[] cipher = new byte[fullCipher.Length - iv.Length];
+
+            Array.Copy(fullCipher, iv, iv.Length);
+            Array.Copy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+            var decryptor = aes.CreateDecryptor(aes.Key, iv);
+
+            using (var ms = new System.IO.MemoryStream(cipher))
+            {
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (var sr = new System.IO.StreamReader(cs))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
 }
